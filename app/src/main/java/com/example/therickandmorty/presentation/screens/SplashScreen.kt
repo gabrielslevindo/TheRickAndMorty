870837bbf55dfe0a6f03011831a999f36caa92ad
Gabriel Levindo <69587938+gabrielslevindo@users.noreply.github.com>
2025-08-22 23:46:19 -0300

    initial Commit

--- app/src/main/java/com/example/therickandmorty/presentation/screens/SplashScreen.kt
+++ app/src/main/java/com/example/therickandmorty/presentation/screens/SplashScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,7 +17,7 @@ import cafe.adriel.voyager.navigator.Navigator
 import com.example.therickandmorty.R
 import kotlinx.coroutines.delay
 
-object SplashScreen : cafe.adriel.voyager.core.screen.Screen {
+object SplashScreen : Screen {
     @Composable
     override fun Content() {
         var startAnimation by remember { mutableStateOf(false) }
